2d3f3b7c429f4c44b29f552a6d016fe13aef7b8f
Cassandra McCarthy <cassie@7596ff.com>
2020-02-25 19:08:37 -0500

    rustfmt; add rss metadata on top

--- src/commands/generate.rs
+++ src/commands/generate.rs
@@ -1,5 +1,7 @@
+use std::io::{self, Write};
+
 use chrono::{NaiveDateTime, Utc};
-use rss::{ChannelBuilder, EnclosureBuilder, Item as RssItem, ItemBuilder};
+use rss::{ChannelBuilder, EnclosureBuilder, GuidBuilder, Item as RssItem, ItemBuilder};
 use rusqlite::{params, Connection};
 
 use crate::{
@@ -20,34 +22,51 @@ pub fn exec(conn: Connection) -> Result<(), Error> {
     // form the rss feed
     let mut channel = ChannelBuilder::default()
         .title("SYSK Forgets")
+        .link("https://gitlab.com/7596ff/sysk-forgets/")
         .description(
             "A feed of all the episodes that Josh and Chuck mention, republished on off-days",
         )
+        .language("en".to_string())
         .build()
         .unwrap();
 
     // get mentioned episode metadata, and add it to a list of items
     let mut items: Vec<RssItem> = vec![];
     for entry in entries {
-        let mentioned = easy_query(&conn, &SELECT_ITEM_BY_GUID, params![entry.mentioned_guid])?;
-        let contained = easy_query(&conn, &SELECT_ITEM_BY_GUID, params![entry.contained_guid])?;
-        let mentioned = &mentioned[0];
-        let contained = &contained[0];
+        let mentioned =
+            easy_query(&conn, &SELECT_ITEM_BY_GUID, params![entry.mentioned_guid])?.remove(0);
+        let contained =
+            easy_query(&conn, &SELECT_ITEM_BY_GUID, params![entry.contained_guid])?.remove(0);
 
-        let originally_published = NaiveDateTime::from_timestamp(contained.pub_date.unwrap(), 0)
-            .format("%Y-%m-%d %H:%M:%S");
+        let contained_pub_date = contained.pub_date.unwrap();
+        let originally_published = NaiveDateTime::from_timestamp(contained_pub_date, 0);
+
         let enclosure = EnclosureBuilder::default()
-            .url(mentioned.enclosure.as_ref().unwrap())
+            .url(mentioned.enclosure.unwrap())
+            .length("0")
+            .mime_type("audio/mpeg")
+            .build()
+            .unwrap();
+
+        let guid = GuidBuilder::default()
+            .value(&originally_published.timestamp().to_string())
             .build()
             .unwrap();
 
         let item = ItemBuilder::default()
             .title(format!("SYSK Forgets: {}", entry.mentioned_title))
             .enclosure(enclosure)
+            .guid(guid)
+            .pub_date(
+                originally_published
+                    .format("%a, %d %b %Y %H:%M:%S -0000")
+                    .to_string(),
+            )
             .content(format!(
-                    "<p>This episode was originally published on {}.</p> {}",
-                    originally_published,
-                    mentioned.content.as_ref().unwrap().to_string()))
+                "<p>This episode was originally published on {}.</p> {}",
+                originally_published.format("%Y-%m-%d %H:%M:%S").to_string(),
+                mentioned.content.unwrap().to_string()
+            ))
             .build()
             .unwrap();
 
@@ -56,7 +75,21 @@ pub fn exec(conn: Connection) -> Result<(), Error> {
 
     channel.set_items(items);
 
-    channel.pretty_write_to(std::io::stdout(), b' ', 4)?;
+    // write to stdout
+    let out = io::stdout();
+    let mut handle = out.lock();
+
+    write!(handle, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")?;
+    write!(handle, "{} {} {} {} {} {}\n",
+        "<rss version=\"2.0\"",
+        "xmlns:itunes=\"http://www.itunes.com/dtds/podcast-1.0.dtd\"",
+        "xmlns:googleplay=\"http://www.google.com/schemas/play-podcasts/1.0\"",
+        "xmlns:atom=\"http://www.w3.org/2005/Atom\"",
+        "xmlns:media=\"http://search.yahoo.com/mrss/\"",
+        "xmlns:content=\"http://purl.org/rss/1.0/modules/content/\">"
+    )?;
+
+    channel.pretty_write_to(handle, b' ', 4)?;
 
     Ok(())
 }
